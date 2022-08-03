88aeac5b161eac72f8c819afbca31c67cfdc86f3
Elyas RAJJAF <93983897+elyasrajjaf@users.noreply.github.com>
2022-08-03 14:15:06 +0200

    Styled Components Home

--- frontend/components/FilmAccueil.js
+++ frontend/components/FilmAccueil.js
@@ -3,13 +3,35 @@ import {formaterDate} from '../helpers/index'
 import styled from "styled-components"
 
 const CardMovie = styled.div`
-    padding: 2rem;
+    padding: 2rem 1.3rem;
     background-color: white;
+    border-radius: 1rem;
 `
+const CardContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 1rem;
+    h2 {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 1;
+        overflow: hidden;
+        margin-top: 1.2rem;
+        font-size: 1.4rem;
+    }
+    div {
+        display: flex;
+        justify-content: space-between;
+        p {
+            color: blue;
+        }
 
+    }
+`
 const FilmAccueil = ({movie}) => {
 
-    const { title, author, description, cover, duration, publishedDate } = movie
+    const { title, author, cover, publishedDate } = movie
 
     return (
         <>
@@ -21,11 +43,13 @@ const FilmAccueil = ({movie}) => {
                     src={`/assets/${cover}`}
                     alt={`Cover de film ${title}`}
                 />
-                <div>
+                <CardContent>
                     <h2>{title}</h2>
-                    <p>{author}</p>
-                    <span>{formaterDate(Number(publishedDate))}</span>
-                </div>
+                    <div>
+                        <p>{author}</p>
+                        <span>{formaterDate(Number(publishedDate))}</span>
+                    </div>
+                </CardContent>
             </CardMovie>
         </>
         
